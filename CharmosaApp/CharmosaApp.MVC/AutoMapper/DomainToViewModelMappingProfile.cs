333b895626973340c83ab0914ad94113ea19dfa8
Administrator <Administrator@CARTOONWEB01>
2019-02-25 15:28:28 -0300

    adicionando controllers e adicionando MVC

---
+++ CharmosaApp/CharmosaApp.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CharmosaApp.MVC.ViewModels;
+using CharmosaAPP.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharmosaApp.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Administrador, AdministradorViewModel>();
+            CreateMap<Funcionario, FuncionarioViewModel>();
+        }
+    }
+}
---
+++ CharmosaApp/CharmosaApp.MVC/ViewModels/FuncionarioViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharmosaApp.MVC.ViewModels
+{
+    public class FuncionarioViewModel
+    {
+        [Key]
+        public int FuncionarioID { get; set; }
+
+        [Required(ErrorMessage ="O campo Nome é obrigatório")]
+        [MaxLength(250,ErrorMessage ="Tamanho máximo é de {0}")]
+        [MinLength(3, ErrorMessage ="Tamanho mínimo é de {0}")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Sobrenome é obrigatório")]
+        [MaxLength(250, ErrorMessage = "Tamanho máximo é de {0} caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho mínimo é de {0} caracteres")]
+
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O campo Salário é obrigatorio")]
+        [Range(0,Double.MaxValue,ErrorMessage ="O salario deve estar entre {0} e {1} reais")]
+        [DisplayName("Salário")]
+        public decimal Salario { get; set; }
+    }
+}
