704f498ff4df4c10bc1e7dc5673d67490a9b3a9c
Pedro Henrique Pires <pedro.henrique@cartoonweb.local>
2019-02-25 12:22:33 -0300

    adicionando interfaces

---
+++ CharmosaApp/CharmosaAPP.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharmosaAPP.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Remove(TEntity obj);
+        void Update(TEntity obj);
+        void Dispose();
+        IEnumerable<TEntity> GetAll();
+        TEntity GetByID(int ID);
+        
+
+    }
+}
---
+++ CharmosaApp/CharmosaApp.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using CharmosaApp.Infra.Data.Contexto;
+using CharmosaAPP.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharmosaApp.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected CharmosaAppContext dbContext;
+        public RepositoryBase(CharmosaAppContext charmosaAppContext)
+        {
+            dbContext = charmosaAppContext;
+        }
+
+        public void Add(TEntity obj)
+        {
+            dbContext.Set<TEntity>().Add(obj);
+            dbContext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dbContext.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetByID(int ID)
+        {
+             return dbContext.Set<TEntity>().Find(ID);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            dbContext.Set<TEntity>().Remove(obj);
+            dbContext.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            dbContext.Entry(obj).State = EntityState.Modified;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
